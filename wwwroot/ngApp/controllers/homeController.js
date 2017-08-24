79b09433d8edf644c4c6f5371f8848f0f8046d9b
Daniella C <d13conley@gmail.com>
2017-08-24 15:31:25 -0700

    attempt to implement filestack

--- wwwroot/ngApp/controllers/homeController.js
+++ wwwroot/ngApp/controllers/homeController.js
@@ -1,5 +1,16 @@
 ﻿class HomeController {
-    constructor() {
+    constructor($accountService, $location) {
         this.message = 'Hello from the home page!';
+
+        
+        this.accountService = $accountService;
+        this.location = $location;
+
+    }
+
+    var client = filestack.init("AdqhVmjnDSXuLRRPEfvdbz");
+
+    pickPhoto() {
+        console.log("picking photo");
     }
 }
