40430a03b040e26c892a72f5ad7ed3dbb80bd579
Janette <jaaylopez3@gmail.com>
2017-08-28 09:55:00 -0700

    other users profiles

---
+++ wwwroot/ngApp/controllers/OtherProfilesController.js
@@ -0,0 +1,15 @@
+﻿class OtherProfilesController {
+    constructor($http) {
+        this.http = $http;
+        this.users = [];
+        this.getUsers();
+        this.users = {};
+    }
+
+    getUsers() {
+        this.http.get("api/Users")
+            .then(res => {
+                this.users = res.data;
+            });
+    }
+}
