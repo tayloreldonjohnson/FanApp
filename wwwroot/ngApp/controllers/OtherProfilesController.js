5128fa14d8885d54bc2af0ed28e6fbad30ec501e
Janette <jaaylopez3@gmail.com>
2017-08-28 13:14:36 -0700

    my things

--- wwwroot/ngApp/controllers/OtherProfilesController.js
+++ wwwroot/ngApp/controllers/OtherProfilesController.js
@@ -1,15 +1,28 @@
 ﻿class OtherProfilesController {
-    constructor($http) {
+    constructor($http, $userService, $state) {
         this.http = $http;
         this.users = [];
-        this.getUsers();
         this.users = {};
+        this.getUsers();
+        this.state = $state;
+        this.userService = $userService;
+        this.email = sessionStorage.getItem("email");
+        this.user;
     }
 
     getUsers() {
         this.http.get("api/Users")
             .then(res => {
                 this.users = res.data;
+                this.state.go("OtherUserProfile");
             });
     }
+    getUser() {
+        this.userService.getUserProfile(this.email)
+            .then((res) => {
+                this.user = res.data;
+                console.log(res.data);
+            });
+    }
+
 }
