665a21963405ce809510905d58a21b6dc8d59381
Daniella C <d13conley@gmail.com>
2017-08-30 14:01:21 -0700

    api test

--- wwwroot/ngApp/controllers/artistProfileController.js
+++ wwwroot/ngApp/controllers/artistProfileController.js
@@ -1,8 +1,14 @@
 ﻿class ArtistProfileController {
-	constructor($ArtistProfileService, $stateParams) {
+	constructor($ArtistProfileService, $stateParams, $http) {
 		this.$ArtistProfileService = $ArtistProfileService;
 		this.name = $stateParams["name"];
-		this.getArtist();
+        this.getArtist();
+        this.clientId = 'client_id=444f00b2a47f4ceeb7f8367170a45960';
+        this.secretKey = 'client_secret=69ca8bd0bd574c7f9e2c321c16c40cdb';
+        this.apiAuthorize = 'https://accounts.spotify.com/authorize';
+        this.$http = $http;
+
+        this.getSpotifyArtist();
 	}
 	getArtist() {
 		this.$ArtistProfileService.getArtist(this.name)
@@ -10,5 +16,17 @@
 				this.artist = res.data;
 				console.log(res.data);
 			});
-	}
+    }
+
+    getSpotifyArtist() {
+        this.$http.get(this.apiAuthorize + "&" + this.clientId)
+            .then((res) => {
+                this.accessToken = res.data;
+                console.log(this.accessToken);
+            })
+    }
+
+
+
+
 }
