8590aaeabe7ed33f11bb1fff0ec255186ab953c2
Rafael Berger <Rafiberger613@gmail.com>
2017-09-08 18:00:43 -0400

    nothing

---
+++ Data/Models/OtherUserVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hello.Data.Models
+{
+    public class OtherUserVM
+    {
+        public string OtherUserId { get; set; }
+        public string OtherEmail { get; set; }
+        public string OtherUserName { get; set; }
+        public string OtherFirstName { get; set; }
+        public string OtherLastName { get; set; }
+        public DateTime OtherDateCreated { get; set; }
+        public string OtherAboutMe { get; set; }
+        public string OtherImageUrl { get; set; }
+        public List<Post> Posts { get; set; }
+
+    }
+}
