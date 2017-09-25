8aa47462d92ccfba0d2d6c4b6cc3ea907e98e2ce
Taylor Johnson <tayloreldonjohnson@gmail.com>
2017-09-25 13:41:27 -0700

    comments

--- wwwroot/ngApp/controllers/homeFeedController.js
+++ wwwroot/ngApp/controllers/homeFeedController.js
@@ -2,41 +2,21 @@
     constructor($http, $stateParams, $state, $uibModal) {
         this.$http = $http;
         this.$state = $state;
-        this.id = $stateParams["id"];
-        sessionStorage.setItem("otherid", this.id);
-        this.otherid = sessionStorage.getItem("otherid");
-        this.post = sessionStorage.getItem("postid");
+		this.id = $stateParams["id"];
+		sessionStorage.setItem("otherid", this.id);
+		this.otherid = sessionStorage.getItem("otherid");
         this.user = sessionStorage.getItem("userid");
-        //this.allPosts = {
-        //    PostId: this.post,
-        //    ApplicationArtistId: this.posts,
-        //    FirstNameOfPersonWhoPosted: "",
-        //    LastNameOfPersonWhoPosted: "",
-        //    ArtistName:"",
-        //    FollowingUserdId: this.user,
-        //    DateCreated: new Date(),
-        //    Media: "",
-        //    Video: "",
-        //    Caption: "",
-        //    ProfileImage:""
-        //};
         this.getPostWithProfile();
         this.like = {
             UserId: this.id,
             PostId: this.post
         };
-        this.cid = $stateParams["id"];
-        sessionStorage.setItem("commentId", this.cid);
-        this.commentId = sessionStorage.getItem("commentId");
-        this.comment = {
-            PostId: this.post,
-            UserId: this.otherid,
-            CommentId: this.cid
-
-        };
         this.getComments();
-        this.$uibModal = $uibModal;
+		this.$uibModal = $uibModal;
+		this.postId = $stateParams["postId"];
     }
+
+	
 
     getPostWithProfile() {
         this.$http.get("api/UserFollowers/postandprofile/" + this.user)
@@ -60,52 +40,44 @@
                 console.log("comments");
             });
     }
-    getComments(commentId) {
-        this.$http.get("api/Comments", { CommentId: commentId })
+    getComments() {
+        this.$http.get("api/Comments/")
             .then(res => {
                 this.comments = res.data;
                 console.log(res.data);
             });
-    }
-    showModalComments() {
-        this.$uibModal.open({
-            templateUrl: '/ngApp/views/modalComments.html',
-            controller: ModalCommentController,
-            controllerAs: 'controller',
-            resolve: {
-                comment : () => this.comment
-            }
-        }).closed.then(() => {
-            // this.addPost();
-        });
-    }
+	}
+
+	showModalComments(postId) {
+		console.log(postId);
+	// find out how to get $state, $http, etc from the angular IOC container, dependency injector
+	// $inject
+		let controller = new ModelCommentController(postId); 
+		this.$uibModal.open({
+			templateUrl: '/ngApp/views/modalComments.html',
+			controller: ModalCommentController,
+			controllerAs: 'controller',
+			resolve: {
+				postId: () => this.postId
+			}
+			// is there another property to add arbitrary data?
+		});
+	}
 }
  
 class ModalCommentController {
-    constructor($stateParams, $http, $state, $uibModalInstance) {
+    constructor(postId, $stateParams, $http, $state, $uibModalInstance) {
         this.$http = $http;
         this.$state = $state;
         this.modal = $uibModalInstance;
-        this.id = $stateParams["id"];
-        sessionStorage.setItem("otherid", this.id);
-        this.otherid = sessionStorage.getItem("otherid");
-        this.post = sessionStorage.getItem("postid");
-        this.user = sessionStorage.getItem("userid");
-        this.cid = $stateParams["id"];
-        sessionStorage.setItem("commentId", this.cid);
-        this.commentId = sessionStorage.getItem("commentId");
-        this.comment;
-       
+		this.postId = $stateParams["postId"]
         this.getComment();
-        //this.getComments();
+	}
 
-        
-    }
-    getComment(commentId, postId, userId, text) {
-        this.$http.get("api/Comments/" + { PostId: postId, Text: text, UserId: this.user, CommentId: commentId })
+    getComment(postId) {
+        this.$http.get("api/Comments/" + postId)
             .then(res => {
-                this.comment = res.data;
-                console.log(res.data);
+                
             });
     }
 }
